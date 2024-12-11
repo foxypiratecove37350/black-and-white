f2791b4930cae04b5dbeeaf5f6a7209ac0e4eae9
foxypiratecove37350 <108185011+foxypiratecove37350@users.noreply.github.com>
2024-12-11 23:38:48 +0100

    Basic Python package project start

---
+++ src/__init__.py
@@ -0,0 +1,12 @@
+"""Black and White: The not that munch compromising Python code formatter
+
+Author: foxy pirate cove / Fnaf
+License: GNU General Public License v2.0 only (GPL-2.0)
+Version: 0.1.0
+"""
+
+__version__ = '0.1.0'
+__author__ = "foxy pirate cove / Fnaf"
+__license__ = "GPL-2.0"
+
+from .core import main
