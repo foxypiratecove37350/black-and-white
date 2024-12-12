012594626f9aaa29b4f0eadc9697cd90cd36cfd4
foxypiratecove37350 <108185011+foxypiratecove37350@users.noreply.github.com>
2024-12-12 18:05:23 +0100

    Change `src/` to `black_and_white/` for the module entry point

---
+++ black_and_white/__main__.py
@@ -0,0 +1,13 @@
+"""Black and White: The not that munch compromising Python code formatter
+
+Author: foxy pirate cove / Fnaf
+License: GNU General Public License v2.0 only (GPL-2.0)
+Version: 0.1.0
+
+Module entry point
+"""
+
+from .cli import main
+
+if __name__ == '__main__':
+	main()
